2b78d1d5a92133120bc18dd641ff2e37e6768e41
Sotiris Tsarouchis <33358541+sotiristsar@users.noreply.github.com>
2017-12-11 12:06:52 +0200

    Add files via upload

--- CDR3.R
+++ CDR3.R
@@ -1,4 +1,13 @@
 # Insert Data
+
+#install.packages("stringr")
+#install.packages("dplyr")
+#install.packages("entropy")
+#install.packages("ggplot2")
+#install.packages("ggseqlogo")
+#install.packages("gridExtra")
+#install.packages("cluster")
+#install.packages("seqinr")
 library("stringr")
 library("dplyr")
 library("entropy")
@@ -6,6 +15,7 @@ library("ggplot2")
 library("ggseqlogo")
 library("gridExtra")
 library("cluster")
+library("seqinr")
 #data <- read.csv(file.choose(), header = TRUE, sep = ";")
 data <- read.csv("data/SampleData.csv", header = TRUE, sep = ";")
 
@@ -30,14 +40,15 @@ udata$level.0 = 0 # Initialize the column of cl.0 with 0
 udata$temp= 0 # Creating a temp column with 0
 d = 0
 nn = FALSE # Initial value for the condition sumper < endper%
-endper = 30 # Set the percentage, which ends the programm 
+endper = 90 # Set the percentage, which ends the programm 
 listxx = list() # Initialize a list for saving the permat of all branches
+dfsum = data.frame(sumper = numeric(0),sumper2 = numeric(0),branch = numeric(0), len = numeric(0)) 
 # Initalize the first list
-listq = list("list" = listxx,"udata" = udata,"permat"= NA, "persim" = NA, "br" = br, "cl" = cl, "met" = met, "ep" = ep , "clep" = clep, "nn" = nn, "sumper" = NA,"sumper2" = NA, "ela" = NA, "cel" = NA)
+listq = list("dfsum" = dfsum,"list" = listxx,"udata" = udata,"permat"= NA, "persim" = NA, "br" = br, "cl" = cl, "met" = met, "ep" = ep , "clep" = clep, "nn" = nn, "sumper" = NA,"sumper2" = NA, "ela" = NA, "cel" = NA,"endper" = endper)
 
 
 # Function Matrices compute the apsolute matrix, the matrix with percentages, the absolute similarity matrix and the percentage similarity matrix
-Matrices <- function(list1){
+Matrices <- function(list1,leaf){
   udata = list1$udata
   permat = list1$permat
   persim = list1$persim
@@ -73,29 +84,48 @@ Matrices <- function(list1){
   }
   listxx$temp = permat
   names(listxx)[length(listxx)] = sprintf('permat_br.%d', br) # Save the permat with this format
-  result=list("list" = listxx, "udata" = udata,"permat"= permat, "persim" = persim, "br" = br, "cl" = cl, "met" = list1$met, "ep" = list1$ep, "clep" = list1$clep, "nn" = list1$nn, "sumper" =list1$sumper,"sumper2" = list1$sumper2, "ela" = list1$ela, "cel" = list1$cel)
-  Finish(result)
+  result1=list("dfsum" = list1$dfsum,"list" = listxx, "udata" = udata,"permat"= permat, "persim" = persim, "br" = br, "cl" = cl, "met" = list1$met, "ep" = list1$ep, "clep" = list1$clep, "nn" = list1$nn, "sumper" =list1$sumper,"sumper2" = list1$sumper2, "ela" = list1$ela, "cel" = list1$cel, "endper" = list1$endper)
+  if ( leaf == TRUE){
+    Finish(result1,TRUE)
+  }else{
+    Finish(result1,FALSE)
+  }
 }
 
 
-Finish <- function(list2){
+Finish <- function(list2,leaf){
   udata = list2$udata
   permat = list2$permat
   persim = list2$persim
-  sumper = list2$sumper
-  sumper2 = list2$sumper2
-  t =which(permat[-length(permat),] == 100,arr.ind = TRUE)
-  sumper = ((20 - length(as.numeric(t[,2])))* 100) / 20
-  if (sumper < 20){
-    nn = TRUE
-    t =which(persim[-length(persim),] == 100,arr.ind = TRUE)
-    sumper2 = ((20 - length(as.numeric(t[,2])))* 100) / 20
-    if (sumper2 < 20){
-      return(list2)
+  sumper = list2$sumper # The total percentage of permat 
+  sumper2 = list2$sumper2 # The total percentage of persim
+  endper = list2$endper
+  dfsum = list2$dfsum
+  br = list2$br
+  t1 =which(permat[-length(permat),] == 100,arr.ind = TRUE)
+  sumper = (length(as.numeric(t1[,2]))* 100) / str_length(udata$AA.JUNCTION[1])
+  #sumper2 = NA
+  t2 =which(persim[-length(persim),] == 100,arr.ind = TRUE)
+  sumper2 = (length(as.numeric(t2[,2]))* 100) / str_length(udata$AA.JUNCTION[1])
+  
+  if (sumper > endper){
+    nn = TRUE  # When nn = TRUE the percentage of sumper < endper%
+    #t2 =which(persim[-length(persim),] == 100,arr.ind = TRUE)
+    #sumper2 = (length(as.numeric(t2[,2]))* 100) / str_length(udata$AA.JUNCTION[1])
+    if (sumper2 > endper){
+      return(list2)  # End of the programm, returning the final list
     }
   }
-  result2 = list("list" = list2$list,"udata" = list2$udata,"permat"= list2$permat, "persim" = list2$persim, "br" = list2$br, "cl" = list2$cl, "met" = list2$met, "ep"= list2$ep, "clep" = list2$clep,"nn" = nn, "sumper" = sumper, "sumper2" = sumper2, "ela" = list2$ela, "cel" = list2$cel)
-  Choice(result2)
+  vv = length(udata[udata$clusters == br,]$AA.JUNCTION)
+  dfsum[nrow(dfsum) + 1,] = c(sumper,sumper2,br,vv)
+  result2 = list("dfsum" = dfsum,"list" = list2$list,"udata" = list2$udata,"permat"= list2$permat, "persim" = list2$persim, "br" = list2$br, "cl" = list2$cl, "met" = list2$met, "ep"= list2$ep, "clep" = list2$clep,"nn" = nn, "sumper" = sumper, "sumper2" = sumper2, "ela" = list2$ela, "cel" = list2$cel,"endper" = list2$endper)
+  if ( leaf == TRUE){
+    nn = FALSE
+    result2 = list("dfsum" = dfsum,"list" = list2$list,"udata" = list2$udata,"permat"= list2$permat, "persim" = list2$persim, "br" = list2$br, "cl" = list2$cl, "met" = list2$met, "ep"= list2$ep, "clep" = list2$clep,"nn" = nn, "sumper" = sumper, "sumper2" = sumper2, "ela" = list2$ela, "cel" = list2$cel,"endper" = list2$endper)
+    return(result2)
+  }else{
+    Choice(result2)
+  }
 }
 
 
@@ -147,7 +177,7 @@ Choice <- function(list3){
     }
   }
   nn = FALSE # Return nn in it's original value 
-  result3 = list("list" = list3$list,"udata" = list3$udata,"permat"= list3$permat, "persim" = list3$persim, "br" = list3$br, "cl" = list3$cl, "met" = list3$met, "ep"= list3$ep, "clep" = list3$clep,"nn" = nn, "sumper" = list3$sumper, "sumper2" = list3$sumper2, "ela" = ela, "cel" = cel)
+  result3 = list("dfsum" = list3$dfsum,"list" = list3$list,"udata" = list3$udata,"permat"= list3$permat, "persim" = list3$persim, "br" = list3$br, "cl" = list3$cl, "met" = list3$met, "ep"= list3$ep, "clep" = list3$clep,"nn" = nn, "sumper" = list3$sumper, "sumper2" = list3$sumper2, "ela" = ela, "cel" = cel,"endper" = list3$endper)
   Divide(result3)
 }
 
@@ -180,8 +210,8 @@ Divide <- function(list4){
   }
   # Updating the cluster column
   udata[udata$clusters == br,]$clusters <- ifelse(str_detect(str_sub(udata[udata$clusters == br,]$AA.JUNCTION,cel[ela,2],cel[ela,2]), let[cel[ela,1]]), cl+1 ,cl+2) 
-  result5 = list("list" = list4$list,"udata" = udata,"permat"= list4$permat, "persim" = list4$persim, "br" = list4$br, "cl" = list4$cl, "met" = list4$met, "ep"= list4$ep, "clep" = list4$clep,"nn" = list4$nn, "sumper" = list4$sumper, "sumper2" = list4$sumper2, "ela" = list4$ela, "cel" = list4$cel)
-  Control(result5)
+  result4 = list("dfsum" = list4$dfsum,"list" = list4$list,"udata" = udata,"permat"= list4$permat, "persim" = list4$persim, "br" = list4$br, "cl" = list4$cl, "met" = list4$met, "ep"= list4$ep, "clep" = list4$clep,"nn" = list4$nn, "sumper" = list4$sumper, "sumper2" = list4$sumper2, "ela" = list4$ela, "cel" = list4$cel,"endper" = list4$endper)
+  Control(result4)
 }
 
 
@@ -200,7 +230,9 @@ Control <- function(list5){
   br = br + 1 # Increase the branch by 1
   cl = cl + 2 # Increase the cluster by 2
   met = met + 2 # Increase the counter by 2
-  while (length(which(udata$clusters == br)) <= 2 && br != cl){ # While the number of sequences in the branch is less than 2, go to the next branch and change counter 
+  list5 = list("dfsum" = list5$dfsum,"list" = list5$list,"udata" = list5$udata,"permat"= list5$permat, "persim" = list5$persim, "br" = br, "cl" = cl, "met" = met, "ep"= ep, "clep" = clep,"nn" = list5$nn, "sumper" = list5$sumper, "sumper2" = list5$sumper2, "ela" = list5$ela, "cel" = list5$cel,"endper" = list5$endper)
+  while (length(which(udata$clusters == br)) <= 2){ # While the number of sequences in the branch is less than 2, go to the next branch and change counter 
+    list5 = Matrices(list5, TRUE)
     if( ((clep[br] < clep[br+1]) && (sum(clep == clep[br]) != (2^clep[br]))) == TRUE ){ # If the next branch is in the next level
       met = 0
       ep = ep + 1
@@ -209,13 +241,14 @@ Control <- function(list5){
       br = br + 1
       met = met +2 
     }
+    list5 = list("dfsum" = list5$dfsum,"list" = list5$list,"udata" = list5$udata,"permat"= list5$permat, "persim" = list5$persim, "br" = br, "cl" = cl, "met" = met, "ep"= ep, "clep" = clep,"nn" = list5$nn, "sumper" = list5$sumper, "sumper2" = list5$sumper2, "ela" = list5$ela, "cel" = list5$cel,"endper" = list5$endper)
   }
   if( met == geomSeq(1,2,1,50)[ep+1]){ # When the counter reaches the end value (geometric sequence) we increase the level counter
     met = 0
     ep = ep + 1
   }
-  result6 = list("list" = list5$list,"udata" = list5$udata,"permat"= list5$permat, "persim" = list5$persim, "br" = br, "cl" = cl, "met" = met, "ep"= ep, "clep" = clep,"nn" = list5$nn, "sumper" = list5$sumper, "sumper2" = list5$sumper2, "ela" = list5$ela, "cel" = list5$cel)
-  Matrices(result6)
+  result5 = list("dfsum" = list5$dfsum,"list" = list5$list,"udata" = list5$udata,"permat"= list5$permat, "persim" = list5$persim, "br" = br, "cl" = cl, "met" = met, "ep"= ep, "clep" = clep,"nn" = list5$nn, "sumper" = list5$sumper, "sumper2" = list5$sumper2, "ela" = list5$ela, "cel" = list5$cel,"endper" = list5$endper)
+  Matrices(result5,FALSE)
 }
 
 
@@ -266,20 +299,42 @@ Opt2 <- function(df){
 }
 
 # A command for beggining our programm 
-lastlist = Matrices(listq)
+lastlist = Matrices(listq,FALSE)
 # The final name of udata data frame 
 df = lastlist$udata
 # A list with the permat matrix for every branch
 perlist = lastlist$list
 
+# Create custom colour scheme
+cs1 = make_col_scheme(chars=c("F","W","A","I","L","V","M","C","P","G","Y","T","S","H","K","R","E","D","Q","N"),
+                      cols=c("#1E90FF", "#BA55D3", "#0000FF", "#0000FF", "#0000FF", "#0000FF", "#C6E2FF", "#C6E2FF", "#FFD700", "#00EE00", "#C1FFC1", "#54FF9F", "#54FF9F", "#FF0000", "#FF0000", "#FF0000", "#FFD700", "#FFD700", "#ED9121", "#ED9121"))
 # Creating a plot for cluster 5 for example
-ggseqlogo(df[df$level.2 == 5,]$AA.JUNCTION)
+ggseqlogo(df[df$level.2 == 5,]$AA.JUNCTION, method = "prob", col_scheme=cs1)
 
+# A function to plot with logo level lev
+Logo <- function(lev){
+  t1 = which(lastlist$clep == lev)
+  listff = list()
+  if(length(t1) %% 3 == 0){
+    nc = length(t1)%/%3
+  }else{
+    nc = length(t1)%/%3 + 1
+  }
+  
+  for(i in 1:length(t1)){
+    x1 = as.data.frame(na.omit(df[df[which(names(df) == sprintf("level.%d", lev))] == t1[i], ]$AA.JUNCTION))
+    names(x1)[1]= "AA.JUNCTION"
+    x1 = as.character(x1$AA.JUNCTION)
+    listff$temp = x1
+    names(listff)[length(listff)] = sprintf('Cluster.%d', t1[i])
+  }
+  ggseqlogo(listff, ncol=nc, method = "prob",col_scheme=cs1)
+}
+
+# Create the grid (we need to miximize the pop-up window in order to have a perfect fit)
+get( getOption( "device" ) )()
 # Creating a plot for level 5 for example
-j = 1
-t1 = which(lastlist$clep == 5)
-get( getOption( "device","maxprint" ) )()
-grid.arrange( ggseqlogo(df[df$level.5 %in% t1[j],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+1],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+2],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+3],]$AA.JUNCTION),ggseqlogo(df[df$level.5 %in% t1[j+4],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+5],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+6],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+7],]$AA.JUNCTION), ggseqlogo(df[df$level.5 %in% t1[j+8],]$AA.JUNCTION),  ggseqlogo(df[df$level.5 %in% t1[j+9],]$AA.JUNCTION),ncol=2)
+Logo(6)
 
 # An alternative plot for cluster 3 (We must determine the cluster's permat)
 par(mar=c(3,3,4,4),xpd=TRUE)
@@ -287,55 +342,61 @@ output <- matrix(unlist(perlist[3]), ncol = 20, byrow = FALSE)
 barplot(output[-nrow(output),], col=heat.colors(length(output[,1])-1), width=2, main = sprintf('Cluster.%d',3)) 
 legend("topright",inset=c(-0.03,-0.23), fill=heat.colors(length(output[,1])-1), legend=let,cex = 0.6)
 
-# Create the grid (we need to miximize the pop-up window in order to have a perfect fit)
-get( getOption( "device" ) )()
 
-# A function to plot level lev
+# A function to plot with barplot level lev
 Bar <- function(lev){
-  tt = which(lastlist$clep == lev)
-  if(length(tt) %% 3 == 0){
-    par(mfrow = c(length(tt)%/%3,3))
+  t2 = which(lastlist$clep == lev)
+  if(length(t2) %% 3 == 0){
+    par(mfrow = c(length(t2)%/%3,3))
   }else{
-    par(mfrow = c(length(tt)%/%3 + 1,3))
+    par(mfrow = c(length(t2)%/%3 + 1,3))
   }
-  for(i in 1:length(tt)){
-    ar = str_which(names(perlist),as.character(tt[i]))[1]
-    if (is.na(ar)){
-      x = as.data.frame(na.omit(df[df[which(names(df) == sprintf("level.%d", lev))] == tt[i], ]$AA.JUNCTION))
-      names(x)[1]= "AA.JUNCTION"
-      vvmat = matrix(0,nrow = 21,ncol = 20)
-      for(j in 1:str_length(x$AA.JUNCTION)){
-        f <- table(str_sub(x$AA.JUNCTION,j,j))
-        if (length(x$AA.JUNCTION) == 1){
-          vvmat[str_which(let,names(f)),j] = 100
-        } else if (length(x$AA.JUNCTION) == 2 && as.numeric(f[1]) == 2){
-          vvmat[str_which(let,names(f)),j] = 100
-        } else{
-          vvmat[str_which(let,names(f[1])),j] = 50
-          vvmat[str_which(let,names(f[2])),j] = 50
-        }
-      }
-      par(xpd=TRUE)
-      barplot(vvmat[-nrow(vvmat),], col=heat.colors(length(vvmat[,1])-1), width=2, main = sprintf('Cluster.%d', tt[i])) 
-      legend("topright",inset=c(-0.03,-0.23), fill=heat.colors(length(vvmat[,1])-1), legend=let,cex = 0.6)
-    }else {
-      par(xpd=TRUE)
-      output <- matrix(unlist(perlist[ar]), ncol = 20, byrow = FALSE)
-      barplot(output[-nrow(output),], col=heat.colors(length(output[,1])-1), width=2, main = sprintf('Cluster.%d', tt[i])) 
-      legend("topright",inset=c(-0.03,-0.23), fill=heat.colors(length(output[,1])-1), legend=let,cex = 0.6)
-    }
+  for(i in 1:length(t2)){
+    ar = str_which(names(perlist),as.character(t2[i]))[1]
+    par(xpd=TRUE)
+    output <- matrix(unlist(perlist[ar]), ncol = 20, byrow = FALSE)
+    barplot(output[-nrow(output),], col=heat.colors(length(output[,1])-1), width=2, main = sprintf('Cluster.%d', t2[i])) 
+    legend("topright",inset=c(-0.03,-0.23), fill=heat.colors(length(output[,1])-1), legend=let,cex = 0.6)
   }
 }
 
+# Create the grid (we need to miximize the pop-up window in order to have a perfect fit)
+get( getOption( "device" ) )()
 # Plot level 5
 Bar(5)
 
-# Correlation for the 12 spot of the sequence for cluster 1 and cluster 2
-cor(perlist$permat_br.2[,12],perlist$permat_br.1[,12])
-# Correlation for the 12 letter(letter = "N" )for cluster 1 and cluster 2
-cor(perlist$permat_br.2[12,],perlist$permat_br.1[12,])
-# The levels for the remaining clusters in column clusters
-d = lastlist$clep[df$clusters]
-# Silhouette for level of cluters and the clusters
-silhouette(df$clusters,d)
 
+# Sequences and Id's for specific cluster
+clust = 15
+lastlist$clep[clust]
+x3 = data.frame(na.omit(df[df[which(names(df) == sprintf("level.%d", lastlist$clep[clust]))] == clust, ]$Sequence.ID), na.omit(df[df[which(names(df) == sprintf("level.%d", lastlist$clep[clust]))] == clust, ]$AA.JUNCTION))
+names(x3) = c("Sequence.ID","AA.JUNCTION")
+se = x3$Sequence.ID
+aa = as.character(x3$AA.JUNCTION)
+aa = as.list(aa)
+names(aa) = se
+x3 # Print in console
+write.fasta(aa, names= names(aa) ,file = sprintf("Cluster.%d", clust)) # Create a fasta file
+Opt(x3) 
+opt2(x3)
+
+# Sequences and Id's for specific level
+level = 7
+x4 = na.omit(as.matrix(df[which((names(df) == sprintf("level.%d", level)))]))
+x4 = df[x4,][1:2]
+se2 = x4$Sequence.ID
+aa2 = as.character(x4$AA.JUNCTION)
+aa2 = as.list(aa2)
+names(aa2) = se2
+x4 # Print in console
+write.fasta(aa2, names= names(aa2) ,file = sprintf("Level.%d", level)) # Create a fasta file
+Opt(x4)
+opt2(x4)
+
+# Plot the identity(sumper)
+ff = lastlist$dfsum
+ff$level[1]= 0
+ff$level[2:(length(lastlist$clep[ff$branch]) +1 )] = lastlist$clep[ff$branch]
+par(xpd=TRUE)
+plot(ff$branch,ff$sumper,col = ff$level, pch = ff$level, main = "Identity Plot", xlab = "Number of Clusters", ylab = "Identity")
+legend("topright",inset=c(-0.05,0.11), col = unique(ff$level), pch = unique(ff$level), legend= unique(sprintf("Level.%d", ff$level)),cex = 0.6)
